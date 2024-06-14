c52c872b77fa6c22c26c9342ee708aae9da8355d
axelayala <axelcapofg@gmail.com>
2024-06-14 01:53:26 -0300

    Inicializacion de clase Carrera

---
+++ src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -0,0 +1,35 @@
+package ar.edu.unju.fi.model;
+
+import lombok.Data;
+
+@Data 
+public class Carrera {
+	private String codigo;
+    private String nombre;
+    private int cantidadAnios;
+    private String estado;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
