e515a251703d81fd7b4750aaf408b5ea1b919dbe
Carlos <Carlos@DESKTOP-LONCR8V>
2024-07-04 00:37:48 -0300

    LopezCarlos: Para las relaciones de la entidades se modificaron el Alumnocontroller, MateriaController, los formularios de cada Entidad

--- src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
+++ src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
@@ -1,72 +1,192 @@
 package ar.edu.unju.fi.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unju.fi.DTO.MateriaDTO;
 import ar.edu.unju.fi.model.Alumno;
+import ar.edu.unju.fi.model.Carrera;
+import ar.edu.unju.fi.model.Materia;
 import ar.edu.unju.fi.service.IAlumnoService;
+import ar.edu.unju.fi.service.ICarreraService;
+import ar.edu.unju.fi.service.IMateriaService;
+import jakarta.validation.Valid;
 
 @Controller
 public class AlumnoController {
+	private static final Log LOGGER = LogFactory.getLog(AlumnoController.class);
 	// Inyeccion de dependencia
-		@Autowired
-		Alumno alumno;
-		@Autowired
-		IAlumnoService alumnoService;
-		
-		@GetMapping("/listaAlumno")
-		public ModelAndView getAlumnos() {
-			ModelAndView modelAndView = new ModelAndView("listaDeAlumnos");
-			modelAndView.addObject("listadoAlumnos", alumnoService.mostrarAlumnos());
-			return modelAndView;
-		}
-		
-		@GetMapping("/formularioAlumno")
-		public ModelAndView getFormAlumno() {
-			ModelAndView modelAndView = new ModelAndView("formAlumno");
-			modelAndView.addObject("nuevoAlumno", alumno);
-			modelAndView.addObject("estado", false);
-			return modelAndView;
-		}
-		
-		@PostMapping("/guardarAlumno")
-		public ModelAndView guardarAlumno(@ModelAttribute("nuevoAlumno") Alumno alumnoNuevoParaGuardar) {
-			alumnoService.guardarAlumno(alumnoNuevoParaGuardar);
-			ModelAndView modelAndView = new ModelAndView("listaDeAlumnos");
-			modelAndView.addObject("listadoAlumnos", alumnoService.mostrarAlumnos());
-			return modelAndView;
-		}
-		
-		  @GetMapping ("/eliminarAlumno/{lu}") 
-		  public ModelAndView borrarAlumno(@PathVariable (name="lu") String lu) {
-			  alumnoService.eliminarAlumno(lu); 
-			  ModelAndView modelAndView = new ModelAndView("listaDeAlumnos"); 
-			  modelAndView.addObject("listadoAlumnos", alumnoService.mostrarAlumnos()); 
-			  return modelAndView; 
-		  }
-		  
-		  @GetMapping("/modificarAlumno/{lu}") 
-		  public ModelAndView getFormModificarAlumno(@PathVariable(name="lu") String lu) { 
-			  Alumno alumnoAModificar = alumnoService.buscarAlumnoPorLu(lu);
-			  ModelAndView modelAndView = new ModelAndView("formAlumno");
-			  
-			  modelAndView.addObject("nuevoAlumno", alumnoAModificar);
-			  modelAndView.addObject("estado", true); 
-			  return modelAndView; 
-		  }
-		  
-		  @PostMapping("/modificarAlumno") 
-		  public ModelAndView modificarAlumno(@ModelAttribute("nuevoAlumno") Alumno alumnoAModificar) { 
-			  alumnoService.modificarAlumno(alumnoAModificar); 
-			  ModelAndView modelAndView = new ModelAndView("listaDeAlumnos");
-			  modelAndView.addObject("listadoAlumnos", alumnoService.mostrarAlumnos());
-		  return modelAndView;
-		  
-		  }
+	@Autowired
+	Alumno alumno;
+	@Autowired
+	Materia materia;
+	@Autowired
+	IAlumnoService alumnoService;
+	@Autowired
+	ICarreraService carreraService;
+	@Autowired
+	IMateriaService materiaService;
 
+	//Alumno alumnoSeleccionado;
+	@GetMapping("/listaAlumno")
+	public ModelAndView getAlumnos() {
+		ModelAndView modelAndView = new ModelAndView("listaDeAlumnos");
+		modelAndView.addObject("listadoAlumnos", alumnoService.mostrarAlumnos());
+		//modelAndView.addObject("listadoCarreras", carreraService.mostrarCarreras());
+		return modelAndView;
+	}
+
+	@GetMapping("/formularioAlumno")
+	public ModelAndView getFormAlumno() {
+		ModelAndView modelAndView = new ModelAndView("formAlumno");
+		modelAndView.addObject("nuevoAlumno", alumno);
+		modelAndView.addObject("listadoCarreras", carreraService.mostrarCarreras());
+		modelAndView.addObject("estado", false);
+		return modelAndView;
+	}
+
+	@PostMapping("/guardarAlumno")
+	public ModelAndView guardarAlumno(@Valid @ModelAttribute("nuevoAlumno") Alumno alumnoNuevoParaGuardar,
+			BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			LOGGER.info("Ocurrió un Error " + alumnoNuevoParaGuardar);
+			ModelAndView modelAndView = new ModelAndView("formAlumno");
+			return modelAndView;
+		}
+		ModelAndView modelAndView = new ModelAndView("redirect:/listaAlumno");
+		try {
+			alumnoNuevoParaGuardar.setCarrera(carreraService.buscarCarreraPorCodigo(alumnoNuevoParaGuardar.getCarrera().getCodigo()));
+			alumnoService.guardarAlumno(alumnoNuevoParaGuardar);
+			LOGGER.info("Se agregó un objeto Alumno a la Base de Datos");
+		} catch (Exception e) {
+			boolean unsave = true;
+			modelAndView.addObject("unsave", unsave);
+			modelAndView.addObject("cargaDeAlumnoErrorMsj", "Error al cargar el nuevo objeto Alumno");
+		}
+		return modelAndView;
+	}
+
+	@GetMapping("/eliminarAlumno/{lu}")
+	public ModelAndView borrarAlumno(@PathVariable(name = "lu") String lu) {
+		alumnoService.eliminarAlumno(lu);
+		ModelAndView modelAndView = new ModelAndView("redirect:/listaAlumno");
+		//ModelAndView modelAndView = new ModelAndView("listaDeAlumnos");
+		//modelAndView.addObject("listadoAlumnos", alumnoService.mostrarAlumnos());
+		return modelAndView;
+	}
+
+	@GetMapping("/modificarAlumno/{lu}")
+	public ModelAndView getFormModificarAlumno(@PathVariable(name = "lu") String lu) {
+		Alumno alumnoAModificar = alumnoService.buscarAlumnoPorLu(lu);
+		ModelAndView modelAndView = new ModelAndView("formAlumno");
+		modelAndView.addObject("nuevoAlumno", alumnoAModificar);
+		modelAndView.addObject("listadoCarreras", carreraService.mostrarCarreras());
+		modelAndView.addObject("estado", true);
+		return modelAndView;
+	}
+
+	@PostMapping("/modificarAlumno")
+	public ModelAndView modificarAlumno(@Valid @ModelAttribute("nuevoAlumno") Alumno alumnoAModificar,
+			BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			LOGGER.info("Ocurrió un Error " + alumnoAModificar);
+			ModelAndView modelAndView = new ModelAndView("formAlumno");
+			modelAndView.addObject("listadoCarreras", carreraService.mostrarCarreras());
+			return modelAndView;
+		}
+
+		ModelAndView modelAndView = new ModelAndView("redirect:/listaAlumno");
+		try {
+			alumnoAModificar.setCarrera(carreraService.buscarCarreraPorCodigo(alumnoAModificar.getCarrera().getCodigo()));
+			alumnoService.modificarAlumno(alumnoAModificar);
+			LOGGER.info("Se modificó un objeto Alumno en la Base de Datos");
+		} catch (Exception e) {
+			boolean errors = true;
+			modelAndView.addObject("errors", errors);
+			modelAndView.addObject("cargaDeAlumnoErrorMsj", "Error al modificar el objeto Alumno");
+		}
+		return modelAndView;
+	}
+	
+	
+	/*Inscripcion**/
+	@GetMapping("/seleccionAlumno")
+	public ModelAndView getFormSeleccionAlumno() {
+		ModelAndView modelAndView = new ModelAndView("formSeleccionAlumno");
+		modelAndView.addObject("alumnos", alumnoService.mostrarAlumnos());
+		modelAndView.addObject("alumno", alumno);
+		return modelAndView;
+	}
+	
+	@PostMapping("/asignarMaterias")
+	public ModelAndView getFormAsignarMaterias(@ModelAttribute("alumno") Alumno alumnoSelec) {
+		ModelAndView modelAndView = new ModelAndView("formAsignarMaterias");
+		System.out.println("alumnoSselleccionado: " + alumnoSelec.getLu());
+		String luAlumno = alumnoSelec.getLu();
+		System.out.println("lu encontrado: " + luAlumno);
+		Alumno alumnoInscribir = alumnoService.buscarAlumnoPorLu(luAlumno);
+		System.out.println("alumno encontrado: " + alumnoInscribir);
+		String codigoC= alumnoInscribir.getCarrera().getCodigo();
+		System.out.println("codigo carrera " + codigoC);
+		Carrera carrera = carreraService.buscarCarreraPorCodigo(codigoC);
+		System.out.println("carrera: " + carrera);
+		//List<MateriaDTO> materias = materiaService.mostrarMaterias();
+		
+		//List<MateriaDTO> materias = (List<MateriaDTO>) materiaService.buscarMateriaPorCarrera(carrera);
+		modelAndView.addObject("alumnoSeleccionado", alumno);
+		//modelAndView.addObject("materias", materias);
+		return modelAndView;
+	}
+	
+	
+	
+	
+	/*Filtrar*/
+	
+	
+	  @GetMapping("/formularioMateriaAlumno")
+		public ModelAndView getFormulario() {
+		ModelAndView modelAndView = new ModelAndView("formMateriaAlumnos");
+		modelAndView.addObject("materia", materia);
+		modelAndView.addObject("materias", materiaService.mostrarMaterias());
+		//System.out.println("carreras: "+ carreraService.mostrarCarreras());
+		return modelAndView;
+	}
+	  
+	  
+	
+	@PostMapping("/filtrarAlumnosPorMateria")
+	public ModelAndView filtrarAlumnos(@ModelAttribute("materia") Materia materiaDeAlumnos) {
+	//public ModelAndView filtrarAlumnos(@RequestParam("codigo") String codigo) {
+	//	System.out.println("codigo que viene: " + codigo);
+    //    Materia materia = materiaService.buscarMateriaPorCodigo(codigo);
+	
+		System.out.println("MATERIA QUE VIENE: " + materiaDeAlumnos);
+	  String codigoC = materiaDeAlumnos.getCodigo();
+	  System.out.println("VALOR DE codigoC: " +codigoC);
+		//Materia materiaAux = materiaService.buscarMateriaPorCodigo(codigoC);
+	//	System.out.println("MATERIA buscada: " + materiaAux);
+	  //List<Alumno> alumnos = materiaService.obtenerAlumnosPorMateria(codigoC);
+		List<Alumno> alumnos = alumnoService.buscarAlumnosPorMateria(materiaService.buscarMateriaPorCodigo(codigoC));
+	  
+	  ModelAndView modelAndView = new ModelAndView("formMateriaAlumnos");
+	 modelAndView.addObject("alumnos", alumnos);
+	  //modelAndView.addObject("alumnos", alumnoService.buscarAlumnosPorMateria(materia));
+	  return modelAndView;  
+}
+
+	
 }
--- src/main/java/ar/edu/unju/fi/controller/MateriaController.java
+++ src/main/java/ar/edu/unju/fi/controller/MateriaController.java
@@ -1,18 +1,29 @@
 package ar.edu.unju.fi.controller;
 
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unju.fi.model.Alumno;
+import ar.edu.unju.fi.model.Carrera;
 import ar.edu.unju.fi.model.Materia;
+import ar.edu.unju.fi.service.ICarreraService;
+import ar.edu.unju.fi.service.IDocenteService;
 import ar.edu.unju.fi.service.IMateriaService;
+import jakarta.validation.Valid;
 
 @Controller
 public class MateriaController {
+	private static final Log LOGGER = LogFactory.getLog(AlumnoController.class);
 
 	// Inyeccion de dependencia
 
@@ -22,56 +33,148 @@ public class MateriaController {
 	@Autowired
 	IMateriaService materiaService;
 	
+	@Autowired
+	IDocenteService docenteService;
+	
+	@Autowired
+	ICarreraService carreraService;
+
 	@GetMapping("/listaMateria")
 	public ModelAndView getMaterias() {
 		ModelAndView modelAndView = new ModelAndView("listaDeMaterias");
 		modelAndView.addObject("listadoMaterias", materiaService.mostrarMaterias());
 		return modelAndView;
 	}
-	
 
 	@GetMapping("/formularioMateria")
 	public ModelAndView getFormMateria() {
 		ModelAndView modelAndView = new ModelAndView("formMateria");
 		modelAndView.addObject("nuevaMateria", materia);
+		modelAndView.addObject("listadoDocentes", docenteService.mostrarDocentes());
+		modelAndView.addObject("listadoCarreras", carreraService.mostrarCarreras());
 		modelAndView.addObject("estado", false);
 		return modelAndView;
 	}
 
 	@PostMapping("/guardarMateria")
-	public ModelAndView guardarMateria(@ModelAttribute("nuevaMateria") Materia materiaNuevaParaGuardar) {
-		materiaService.guardarMateria(materiaNuevaParaGuardar);
+	public ModelAndView guardarMateria(@Valid @ModelAttribute("nuevaMateria") Materia materiaNuevaParaGuardar,
+			BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			LOGGER.info("Ocurrió un Error: formulariuo Incompleto " + materiaNuevaParaGuardar);
+			ModelAndView modelAndView = new ModelAndView("formMateria");
+			return modelAndView;
+		}
+		
+		ModelAndView modelAndView = new ModelAndView("redirect:/listaMateria");
+		
+		try {
+			materiaNuevaParaGuardar.setDocente(docenteService.buscarDocentePorLegajo(materiaNuevaParaGuardar.getDocente().getLegajo()));
+			materiaNuevaParaGuardar.setCarrera(carreraService.buscarCarreraPorCodigo(materiaNuevaParaGuardar.getCarrera().getCodigo()));
+			materiaService.guardarMateria(materiaNuevaParaGuardar);
+			LOGGER.info("Se agregó un objeto Materia en la Base de Datos");
+		} catch (Exception e) {
+			boolean unsave = true;
+			modelAndView.addObject("unsave", unsave);
+			modelAndView.addObject("cargaDeMateriaErrorMsj", "Error al cargar el nuevo objeto Materia");
+
+			
+		}
+		// ModelAndView modelAndView = new ModelAndView("listaDeMaterias");
+		// modelAndView.addObject("listadoMaterias", materiaService.mostrarMaterias());
+		return modelAndView;
+	}
+
+	@GetMapping("/eliminarMateria/{codigo}")
+	public ModelAndView borrarMateria(@PathVariable(name = "codigo") String codigo) {
+		materiaService.eliminarMateria(codigo);
 		ModelAndView modelAndView = new ModelAndView("listaDeMaterias");
 		modelAndView.addObject("listadoMaterias", materiaService.mostrarMaterias());
 		return modelAndView;
 	}
+
+	@GetMapping("/modificarMateria/{codigo}")
+	public ModelAndView getFormModificarMateria(@PathVariable(name = "codigo") String codigo) {
+		Materia materiaAModificar = materiaService.buscarMateriaPorCodigo(codigo);
+		
+		ModelAndView modelAndView = new ModelAndView("formMateria");
+		modelAndView.addObject("nuevaMateria", materiaAModificar);
+		modelAndView.addObject("estado", true);
+		
+		modelAndView.addObject("listadoDocentes", docenteService.mostrarDocentes());
+		modelAndView.addObject("listadoCarreras", carreraService.mostrarCarreras());
+		
+		
+		return modelAndView;
+	}
+
+	@PostMapping("/modificarMateria")
+	public ModelAndView modificarMateria(@Valid @ModelAttribute("nuevaMateria") Materia materiaAAModificar,
+			BindingResult bindingResult) { 
+		if (bindingResult.hasErrors()) {
+			LOGGER.info("Ocurrió un Error: formulariuo Incompleto " + materiaAAModificar);
+			ModelAndView modelAndView = new ModelAndView("formMateria");
+			
+			modelAndView.addObject("nuevaMateria", materiaAAModificar);
+			
+			
+			return modelAndView;
+		}
+		ModelAndView modelAndView = new ModelAndView("redirect:/listaMateria");
+		
+		try {
+			materiaAAModificar.setDocente(docenteService.buscarDocentePorLegajo(materiaAAModificar.getDocente().getLegajo()));
+			materiaAAModificar.setCarrera(carreraService.buscarCarreraPorCodigo(materiaAAModificar.getCarrera().getCodigo()));
+			  materiaService.modificarMateria(materiaAAModificar);
+			  LOGGER.info("Se modificó un objeto Materia en la Base de Datos");
+			} catch (Exception e) {
+				boolean unsave = true;
+				modelAndView.addObject("unsave", unsave);
+				modelAndView.addObject("cargaDeMateriaErrorMsj", "Error al modificar el objeto Materia");
+			}
+		
+		//ModelAndView modelAndView = new ModelAndView("listaDeMaterias");
+		//modelAndView.addObject("listadoMaterias", materiaService.mostrarMaterias());
+		return modelAndView;
+
+	}
 	
-	  @GetMapping ("/eliminarMateria/{codigo}") 
-	  public ModelAndView borrarMateria(@PathVariable (name="codigo") String codigo) {
-		  materiaService.eliminarMateria(codigo); 
-		  ModelAndView modelAndView = new ModelAndView("listaDeMaterias"); 
-		  modelAndView.addObject("listadoMaterias", materiaService.mostrarMaterias()); 
-		  return modelAndView; 
+	/*
+		  @GetMapping("/formularioMateriaAlumno")
+	  public ModelAndView getFormulario() {
+			ModelAndView modelAndView = new ModelAndView("formMateriaAlumnos");
+			modelAndView.addObject("materia", materia);
+			modelAndView.addObject("materias", materiaService.mostrarMaterias());
+			System.out.println("carreras: "+ carreraService.mostrarCarreras());
+			return modelAndView;
+		}
+		
+	  @PostMapping("/filtrarAlumnosPorMateria")
+	  public ModelAndView filtrarAlumnos(@ModelAttribute("materia") Materia materiaDeAlumnos) {
+		 // String codigoC = materiaDeAlumnos.getCodigo();
+		 // List<Alumno> alumnos = materiaService.obtenerAlumnosPorMateria(materiaDeAlumnos.getCodigo());
+		  ModelAndView modelAndView = new ModelAndView("formMateriaAlumnos");
+		  modelAndView.addObject("alumnos", materiaService.obtenerAlumnosPorMateria(materiaDeAlumnos.getCodigo()));
+		  return modelAndView;  
 	  }
-	  
-	  @GetMapping("/modificarMateria/{codigo}") 
-	  public ModelAndView getFormModificarMateria(@PathVariable(name="codigo") String codigo) { 
-		  System.out.println("codigo que viene: " + codigo);
-		  Materia materiaAModificar = materiaService.buscarMateriaPorCodigo(codigo);
-		  System.out.println("codigo que vuelve: " + materiaAModificar.getCodigo());
-		  ModelAndView modelAndView = new ModelAndView("formMateria");
-		  modelAndView.addObject("nuevaMateria", materiaAModificar);
-		  modelAndView.addObject("estado", true); 
-		  return modelAndView; 
-	  }
-	  
-	  @PostMapping("/modificarMateria") 
-	  public ModelAndView modificarMateria(@ModelAttribute("nuevaMateria") Materia materiaaAModificar) { 
-		  materiaService.modificarMateria(materiaaAModificar); 
-		  ModelAndView modelAndView = new ModelAndView("listaDeMaterias");
-		  modelAndView.addObject("listadoMaterias", materiaService.mostrarMaterias());
-	  return modelAndView;
-	  
-	  }
+	
+	
+	*/
+	
+	
+	
+	
+	
+	
+	/**/
+	/*@GetMapping("/mostrarMateriasDelAlumno")
+	
+	public ModelAndView getMateriasDelAlumno(@ModelAttribute("lu") String lu) {
+		Carrera carrera = carreraService.;
+		
+		ModelAndView modelAndView = new ModelAndView("formSeleccionMaterias");
+		modelAndView.addObject("materias", carrera.getMaterias());
+		return modelAndView;
+	}
+	*/
 
 }
